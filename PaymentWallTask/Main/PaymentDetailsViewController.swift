cde456437a3e1f1646865071dacd0b0072c087de
Antoun <antounesmat@gmail.com>
2020-08-23 03:19:56 +0200

    Payment Details UI Done

---
+++ PaymentWallTask/Main/PaymentDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PaymentDetailsViewController.swift
+//  PaymentWallTask
+//
+//  Created by Antoun on 23/08/2020.
+//  Copyright © 2020 PaymentWall. All rights reserved.
+//
+
+import UIKit
+
+class PaymentDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
